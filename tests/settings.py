76634d033e434d652d6e2c223db5fa2b5e3dbd1d
Thomas Khyn <thomas@ksytek.com>
2014-04-09 09:55:31 +1200

    Setup test environment

---
+++ tests/settings.py
@@ -0,0 +1,17 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'NAME': 'jsdir',
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'jsdir',
+    'tests',
+)
