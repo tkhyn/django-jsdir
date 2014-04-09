d3be7fb97cc77952e960159e00d2bb89bedf770f
Thomas Khyn <thomas@ksytek.com>
2014-04-09 17:28:53 +1200

    Added runserver management command

---
+++ jsdir/management/commands/runserver.py
@@ -0,0 +1,11 @@
+from django.contrib.staticfiles.management.commands.runserver \
+    import Command as StaticFilesCommand
+
+from jsdir.base import JSDir
+
+
+class Command(StaticFilesCommand):
+
+    def __init__(self):
+        JSDir.set_use_finders(True)  # sets the value only for this thread
+        super(Command, self).__init__()
